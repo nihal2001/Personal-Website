115eeea1c0361b90171efe68867696639854e989
Nihal <nihal@admins-mbp-2.lan>
2023-05-24 01:58:09 -0500

    Added styling and content for projects

--- personalwebsite/src/pages/About.js
+++ personalwebsite/src/pages/About.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './Pages.css';
-import aboutImage from '../images/sky.png';
 
 
 const About = () => (
     <div className="about-container">
-        <img className="about-image" src={aboutImage} alt="The sky" />
+        <h1>About Me</h1>
+        <p>
+            Hello, my name is Nihal, and I am a recent graduate with a Bachelor's Degree in Computer Science from Virginia Tech.
+            Driven by an unwavering passion for technology and innovation, I seek to utilize my skills in programming, and my knowledge of computer science, 
+            to drive the advancement of technology in today's ever-evolving digital landscape.
+            During my time at Virginia Tech, I developed a solid foundation in computer science theories and principles, 
+            specializing in areas such as software development, machine learning, and algorithmic analysis. 
+            With a keen interest in machine learning and digital signal proccessing, I'm eager and interested to contribute to impactful projects that promote creativity in the music production space. 
+        </p>
+
+        <p>
+            Thank you for taking the time to visit my website. Please don't hesitate to get in touch if you'd like to discuss potential projects, collaboration opportunities, or if you just want to connect and chat about all things tech! You can reach me at nihal.mitta@gmail.com or through my social media channels linked below.
+
+            I'm excited to embark on this journey in the world of Computer Science and look forward to making valuable connections along the way
+        </p>
     </div>
 );
  
--- personalwebsite/src/pages/Projects.js
+++ personalwebsite/src/pages/Projects.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import './Pages.css';
+import graph from '../images/project-graph.png';
 
-const Projects = () => <h1>Projects Page</h1>;
+
+const Projects = () => {
+    return (
+        <div className="projects">
+            <div className="project-card">
+                <img className="project-image" src={graph} alt="Graph" />   
+                <div className="text-container">
+                    <h2>Financial News Sentiment Analysis</h2>
+                    <p>Sentiment analysis for financial statements, specifically found in financial news articles or headlines.</p>
+                </div>
+            </div>
+            <div className="project-card">
+                
+                <h2>Project 2</h2>
+                <p>This is a summary of project 2.</p>
+                
+            </div>
+        </div>
+    );
+}
 
 export default Projects;
