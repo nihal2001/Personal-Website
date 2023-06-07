6dd953d624171490c8a82fd141fadb9ecafe580f
Nihal <nihal@admins-mbp-2.lan>
2023-06-06 23:16:36 -0500

    Stylized contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,8 +3,14 @@ import './Pages.css';
 import ContactForm from '../components/ContactForm';
 
 const Contact = () => (
-    <div>
-        <ContactForm />
+    <div className="contact-container">
+        <div className="contact-text">
+            <h1>Send a message</h1>
+        </div>
+        <div className="contact-form">
+            <ContactForm />
+        </div>
     </div>
 );
+
 export default Contact;
